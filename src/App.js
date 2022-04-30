5e40040654274e30184ddfaa650c0cea7d8c6c5c
XEmAX32 <ema.sacco32@gmail.com>
2022-04-30 04:31:22 +0200

    socket test + qrcode reader test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from "socket.io-client";
+import { QrReader } from 'react-qr-reader';
+const ENDPOINT = "http://192.168.223.145:3232";
 
 function App() {
+  const [response, setResponse] = useState("");
+  const [data, setData] = useState('No result');
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          god have spoken: {response}.
+          qrcode: {data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      {/* <video
+        id="qr-code"
+      >
+
+      </video>
+      <QrReader
+        videoId="qr-code"
+        onResult={(result, error) => {
+          if (!!result) {
+            console.log(result);
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            //console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      /> */}
     </div>
   );
 }
