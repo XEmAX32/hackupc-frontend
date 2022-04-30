7e48a3033ba8189581b1f9dab905c2037ee3baae
XEmAX32 <ema.sacco32@gmail.com>
2022-04-30 11:44:05 +0200

    qrcode

--- src/App.js
+++ src/App.js
@@ -2,49 +2,75 @@ import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import socketIOClient from "socket.io-client";
-import { QrReader } from 'react-qr-reader';
+import { QrReader } from '@blackbox-vision/react-qr-reader';
+import adapter from 'webrtc-adapter';
 const ENDPOINT = "http://192.168.223.145:3232";
 
 function App() {
-  const [response, setResponse] = useState("");
+  let socket;
+  const [status, setStatus] = useState("not-logged-in");
+  const [response, setResponse] = useState();
   const [data, setData] = useState('No result');
+
+  const forwardUser = (userId) => {
+    console.log(userId)
+    socket.emit("register", {id: userId})
+  }
+
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("FromAPI", data => {
+    socket = socketIOClient(ENDPOINT, {
+      reconnection: true,
+      reconnectionDelay: 500,
+      reconnectionAttempts: 10,
+    });
+    socket.on("connect", data => {
+      console.log(data)
+    });
+    socket.on("message", data => {
+      console.log(data)
       setResponse(data);
     });
-
+    socket.on("disconnect", data => {
+      console.log(data)
+    });
   }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          god have spoken: {response}.
-          qrcode: {data}
-        </p>
-      </header>
-      {/* <video
-        id="qr-code"
-      >
 
-      </video>
-      <QrReader
-        videoId="qr-code"
-        onResult={(result, error) => {
-          if (!!result) {
-            console.log(result);
-            setData(result?.text);
-          }
+  if(status == "not-logged-in") {
+    console.log('sta',status);
+    return (
+      <div className="App">
+        {/* <video
+          id="qr-code"
+          width="100%"
+          height="100%"
+        >
+        </video> */}
+        <QrReader
+          // videoId="qr-code"
+          ViewFinder={() => <div style={{position: 'absolute', top:200, zIndex: 9999}}>test</div>}
+          videoContainerStyle={{margin: 0}}
+          onResult={(result, error) => {
+            if (!!result) {
+              console.log(result);
+              setData(result?.text);
+              forwardUser(result?.text);
+            }
 
-          if (!!error) {
-            //console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      /> */}
-    </div>
-  );
+            if (!!error) {
+              //console.info(error);
+            }
+          }}
+        />
+      </div>
+    );
+  } else {
+    console.log('other')
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
