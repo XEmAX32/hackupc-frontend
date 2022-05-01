f5aebc454130fd6b369a993e5e3af19495504475
Luigi Masini <luigimasini.lm@gmail.com>
2022-05-01 05:12:38 +0200

    chs jdg xsSL

--- src/Wall.js
+++ src/Wall.js
@@ -3,7 +3,6 @@ import Question from './Question'
 import { socket } from './socketWorker.js'
 import Win from './Win';
 import NoKey from './NoKey';
-import ImageMap from './ImageMap';
 
 function Wall ({image, objects, setTime}) {
   useEffect(() => {
@@ -13,29 +12,30 @@ function Wall ({image, objects, setTime}) {
 
 	return (
 		<div id="wallContainer">
-      <ImageMap imgpath={process.env.REACT_APP_IMG_SERVER_ADDRESS+image}/>
 
-			{ !objects ?
-				null :
-				objects.map( config => <Object {...config}/>)
-			}
+			<svg viewBox="0 0 2500 1767" style={{height:'100%'}}>
+				<image width={2500} height={1767} href={process.env.REACT_APP_IMG_SERVER_ADDRESS+image} />
+
+				{ !objects ?
+					null :
+					objects.map( config => <Object {...config}/>)
+				}
+
+			</svg>
 		</div>
 	)
 }
 
 function Object ({x, y, width, height, question, item}) {
 
+	console.log(x, y, width, height)
+
 	const [isOpen, setIsOpen] = useState(false);
-  const [popupType, setPopupType] = useState(0);
+	const [popupType, setPopupType] = useState(0);
 
 	return (
 		<>
-			<div
-				className="object"
-				style={{left:x, top:y, width, height}}
-				onClick={() => !isOpen && setIsOpen(true)}
-				>
-			</div>
+			<rect x={x} y={y} width={width} height={height} fill={"#fff"} opacity={0} onClick={() => !isOpen && setIsOpen(true)}/>
 
 			<div className={"popupContainer" + (isOpen ? ' open': '')}>
 				{popupType == 0 ? <Question img={item.image} question={question.text} options={question.answers} correct={question.answer} successfullCallback={() => {setPopupType(1)}} errouneousCallback={() => {setPopupType(2)}}/> : (popupType == 1 ? <Win /> : <NoKey />)}
