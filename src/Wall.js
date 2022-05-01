c6a67f0b90e6964317b30817f115dcb3d4bf1eb8
XEmAX32 <ema.sacco32@gmail.com>
2022-05-01 05:12:22 +0200

    minchia

--- src/Wall.js
+++ src/Wall.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import Question from './Question'
 import { socket } from './socketWorker.js'
-import Win from './Win';
+import FoundKey from './FoundKey';
 import NoKey from './NoKey';
 import ImageMap from './ImageMap';
 
@@ -25,20 +25,49 @@ function Wall ({image, objects, setTime}) {
 
 function Object ({x, y, width, height, question, item}) {
 
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(true);
   const [popupType, setPopupType] = useState(0);
+
+  const renderPopup = () => {
+    if(popupType == 0)
+      return (
+        <Question 
+          img={item.image} 
+          question={question.text} 
+          options={question.answers} 
+          correct={question.answer} 
+          successfullCallback={() => {
+              if(item.keys)
+                setPopupType(2)
+              else
+                setPopupType(2)
+          }} 
+          errouneousCallback={() => {alert('Wrong answer: retry later!')}} 
+          close={() => setIsOpen(false)}
+        />
+      );
+    else if(popupType == 1)
+      return (
+        <FoundKey text={item.text} img={process.env.REACT_APP_IMG_SERVER_ADDRESS+item.image} close={() => setIsOpen(false)}/>
+      )
+    else if(popupType == 2)
+      return (
+        <NoKey text={item.text} img={process.env.REACT_APP_IMG_SERVER_ADDRESS+item.image} close={() => setIsOpen(false)}/>
+      )
+  }
 
 	return (
 		<>
-			<div
+			{/* <div
 				className="object"
-				style={{left:x, top:y, width, height}}
+				style={{left: x, top: y, width, height}}
 				onClick={() => !isOpen && setIsOpen(true)}
 				>
-			</div>
+			</div> */}
 
 			<div className={"popupContainer" + (isOpen ? ' open': '')}>
-				{popupType == 0 ? <Question img={item.image} question={question.text} options={question.answers} correct={question.answer} successfullCallback={() => {setPopupType(1)}} errouneousCallback={() => {setPopupType(2)}}/> : (popupType == 1 ? <Win /> : <NoKey />)}
+        {renderPopup()}
+				{/* {popupType == 0 ? <Question img={item.image} question={question.text} options={question.answers} correct={question.answer} successfullCallback={() => {setPopupType(1)}} errouneousCallback={() => {alert('Wrong answer: retry later!')}} close={() => setIsOpen(false)}/> : (popupType == 1 ? <FoundKey close={() => setIsOpen(false)}/> : <NoKey close={() => setIsOpen(false)}/>)} */}
 			</div>
 		</>
 	)
